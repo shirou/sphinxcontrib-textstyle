34bab1db44ddf8f8d1aea2e9ea281f41410d18fd
WAKAYAMA Shirou <shirou.faw@gmail.com>
2013-03-13 15:59:12 +0900

    version 0.2.0.

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ long_desc = '''
 This package contains the text style Sphinx extension.
 
 - ruby (ruby tag. not ruby-lang)
-- del
+- del (role and directive)
+- color
 
 '''
 
@@ -14,7 +15,7 @@ requires = ['Sphinx>=0.6', 'setuptools']
 
 setup(
     name='sphinxcontrib-textstyle',
-    version='0.1.0',
+    version='0.2.0',
     url='http://bitbucket.org/r_rudi/sphinxcontrib-textstyle',
     download_url='http://pypi.python.org/pypi/sphinxcontrib-textstyle',
     license='BSD',
