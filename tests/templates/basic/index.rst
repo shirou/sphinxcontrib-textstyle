8fa2aa3ff4b38f08c6520cb89417127e0b4a94f8
tk0miya <i.tkomiya@gmail.com>
2016-11-29 19:43:26 +0900

    Add test script

---
+++ tests/templates/basic/index.rst
@@ -0,0 +1,8 @@
+Test doc for textstyle
+======================
+
+A text having ruby: :ruby:`東京<とうきょう>`
+
+A deleted text: :del:`Delete`
+
+A colored text: :color:`Color changed<red>`
---
+++ tests/test_textstyle.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from sphinx_testing import with_app
+
+
+@with_app(srcdir='tests/templates/basic')
+def test_build_html(app, status, warnings):
+    app.build()
+    print(status.getvalue(), warnings.getvalue())
+
+    html = (app.outdir / 'index.html').read_text()
+    print(html)
+
+    # :ruby: role
+    expected = ('A text having ruby: <ruby>\n'
+                '<rb>\n'
+                '東京</rb><rp>\n'
+                '(</rp><rt>\n'
+                'とうきょう</rt><rp>\n'
+                ')</rp></ruby>')
+    assert expected in html
+
+    # :del: role
+    expected = ('A deleted text: <del>\n'
+                'Delete</del>')
+    assert expected in html
+
+    # :color: role
+    expected = 'A colored text: <span style="color: red">Color changed</span>'
+    assert expected in html
