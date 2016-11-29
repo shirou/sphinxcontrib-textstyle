346a0b7fe8838ac195aaf1f6036a8da966b4fd7b
tk0miya <i.tkomiya@gmail.com>
2016-11-29 21:35:15 +0900

    Use dict-like attributes to store ruby data instead

--- sphinxcontrib/rubytag.py
+++ sphinxcontrib/rubytag.py
@@ -15,13 +15,13 @@ def visit_rubytag_node(self, node):
 
     self.body.append(self.starttag(node, 'ruby', suffix=''))
     self.body.append(self.starttag(node, 'rb', suffix=''))
-    self.body.append(node.rb)
+    self.body.append(node['base'])
     self.body.append('</rb>')
     self.body.append(self.starttag(node, 'rp', suffix=''))
     self.body.append(paren_start)
     self.body.append('</rp>')
     self.body.append(self.starttag(node, 'rt', suffix=''))
-    self.body.append(node.rt)
+    self.body.append(node['text'])
     self.body.append('</rt>')
     self.body.append(self.starttag(node, 'rp', suffix=''))
     self.body.append(paren_end)
@@ -33,16 +33,14 @@ def visit_rubytag_node(self, node):
 def rubytag_role(name, rawtext, text, lineno, inliner, options={}, content=[]):
     """Role for rubytag."""
     text = utils.unescape(text)
-    has_explicit, rb, rt = split_explicit_title(text)
+    has_explicit, base, text = split_explicit_title(text)
 
     if not has_explicit:
         # the role does not have ruby-text is converted to Text node
         text = nodes.Text(text)
         return [text], []
     else:
-        rubytag = RubyTag()
-        rubytag.rb = rb
-        rubytag.rt = rt
+        rubytag = RubyTag(rawtext, base=base, text=text)
         return [rubytag], []
 
 
